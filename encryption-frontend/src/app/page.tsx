5e4637af22fe3444c481e2a8b317b7ecc45c2876
j-wielg <jwielgus@nd.edu>
2025-04-22 18:01:30 -0400

    refactor: Moved code to another module to prevent cluttering Home

--- encryption-frontend/src/app/page.tsx
+++ encryption-frontend/src/app/page.tsx
@@ -13,7 +13,7 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import { vigenereEncrypt, vigenereDecrypt } from "@/utils/vigenere";
-import { aesEncrypt, aesDecrypt } from "@/utils/aes";
+import { AES } from "@/components/aes";
 
 export default function Home() {
   const [vigenereKey, setVigenereKey] = useState("");
@@ -23,14 +23,6 @@ export default function Home() {
   const [rsaPrivateKey, setRsaPrivateKey] = useState("");
   const [rsaText, setRsaText] = useState("");
   const [rsaResult, setRsaResult] = useState("");
-  const [aesKey, setAesKey] = useState("");
-  const [aesKeyInputStatus, setAesKeyInputStatus] = useState("");
-  const [aesKeyWarning, setAesKeyWarning] = useState(false);
-  const [aesKeySize, setAesKeySize] = useState(128);
-  const [blockMode, setBlockMode] = useState(0);
-  const [aesData, setAesData] = useState("");
-  const [aesDataType, setAesDataType] = useState("ascii");
-  const [aesResult, setAesResult] = useState("");
 
   const handleVigenereEncrypt = () => {
     const encrypted = vigenereEncrypt(vigenereText, vigenereKey);
@@ -51,80 +43,6 @@ export default function Home() {
     // TODO: Implement RSA decryption
     setRsaResult("Decrypted: " + rsaText);
   };
-
-  const handleAesKeyEnter = (value: string) => {
-    setAesKey(value);
-    if (value.length == 0) {
-      setAesKeyInputStatus("");
-      setAesKeyWarning(false);
-      return;
-    }
-    let len = 0;
-    if (value.startsWith("0x")) {
-      let valid = "0123456789abcdefABCDEF _-\n"
-      for (let c of value.slice(2)) {
-        if (!valid.includes(c)) {
-          setAesKeyInputStatus("Character '" + c + "' is invalid for hexadecimal input");
-          return;
-        } else if (c != ' ' && c != '_' && c != '-' && c != '\n') {
-          ++len;
-        }
-      }
-    } else if (value.startsWith("0b")) {
-      let valid = "01 _-\n"
-      for (let c of value.slice(2)) {
-        if (!valid.includes(c)) {
-          setAesKeyInputStatus("Character '" + c + "' is invalid for binary input");
-          return;
-        } else if (c != ' ' && c != '_' && c != '-' && c != '\n') {
-          ++len;
-        }     
-      }
-    } else {
-      setAesKeyInputStatus("Key must be in either binary or hexadecimal format");
-      return;
-    }
-    if (value.startsWith("0x")) {
-      if (len < aesKeySize / 4) {
-        setAesKeyWarning(true);
-        setAesKeyInputStatus("");
-      } else if (len == aesKeySize / 4) {
-        setAesKeyWarning(false);
-        setAesKeyInputStatus("");
-      } else {
-        setAesKeyWarning(false);
-        setAesKeyInputStatus("Key cannot exceed " + aesKeySize.toString() + " bits");
-      }
-    } else if (value.startsWith("0b")) {
-      if (len < aesKeySize) {
-        setAesKeyWarning(true);
-        setAesKeyInputStatus("");
-      } else if (len == aesKeySize) {
-        setAesKeyWarning(false);
-        setAesKeyInputStatus("");
-      } else {
-        setAesKeyWarning(false);
-        setAesKeyInputStatus("Key cannot exceed " + aesKeySize.toString() + " bits");
-      }
-
-    } else {
-      setAesKeyWarning(false);
-      setAesKeyInputStatus("");
-    }
-  }
-
-  const handleAesEncrypt = () => {
-    // Step 1: Turn the key string into a bigint
-    // Step 2: Turn the data string into an array of bytes
-    //      Step 2a: If the data string is binary/hex, parse it
-    //      Step 2b: If the data string is ASCII, get the char codes
-    // Step 3: Do the encryption
-    // Step 4: Print the encrypted blocks
-  }
-
-  const handleAesDecrypt = () => {
-
-  }
 
   return (
     <div className="container mx-auto p-4 max-w-4xl">
@@ -241,83 +159,7 @@ export default function Home() {
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-4">
-              <div className="space-y-2">
-                <Label htmlFor="aes-type">AES Key Size</Label>
-                <select 
-                  className="bg-muted px-2 py-2 outline"
-                  onChange={(e) => setAesKeySize(Number(e.target.value))}
-                >
-                  <option value="128">AES-128</option>
-                  <option value="192">AES-192</option>
-                  <option value="256">AES-256</option>
-                </select>
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="aes-mode">AES Block Mode</Label>
-                <select 
-                  className="bg-muted px-2 py-2 outline"
-                  onChange={(e) => setBlockMode(Number(e.target.value))}
-                >
-                  <option value="0">Electronic Codebook</option>
-                  <option value="1">Cipherblock Chaining</option>
-                  <option value="2">Counter</option>
-                </select>
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="aes-datatype">Data Type</Label>
-                <select 
-                  className="bg-muted px-2 py-2 outline"
-                  onChange={(e) => setAesDataType(e.target.value)}
-                >
-                  <option value="ascii">ASCII Text</option>
-                  <option value="binary">Binary</option>
-                </select>
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="aes-key">AES Key</Label>
-                <TextArea
-                  id="aes-key"
-                  value={aesKey}
-                  onChange={(e) => handleAesKeyEnter(e.target.value)}
-                  placeholder="Preface binary with 0b and hex with 0x"
-                  className={(aesKey.length > 0) ? "font-mono" : ""}
-                />
-                {aesKeyInputStatus.length > 0 && (
-                  <div>
-                    <p className="text-red-700 text-sm">{aesKeyInputStatus}</p>
-                  </div>
-                )}
-                {aesKeyWarning && aesKeyInputStatus.length == 0 && (
-                  <div>
-                    <p className="text-yellow-700 text-sm">Keys under {aesKeySize} bits will be left-padded with zeros</p>
-                  </div>
-                )}
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="aes-data">Data</Label>
-                <TextArea
-                  id="aes-data"
-                  value={aesData}
-                  onChange={(e) => setAesData(e.target.value)}
-                  className={(aesDataType == "binary" && aesData.length > 0) ? "font-mono" : ""}
-                  placeholder={
-                    (aesDataType == "binary") ?
-                    "Enter hex (0x) or binary (0b) data" :
-                    "Enter text to encrypt here"
-                  }
-                />
-              </div>
-              <div className="flex gap-2">
-                { (aesKeyInputStatus.length == 0) &&
-                  <Button onClick={handleAesEncrypt}>Encrypt</Button>
-                }
-                { (aesDataType != "ascii") &&
-                  (aesKeyInputStatus.length == 0) &&
-                  <Button variant="outline" onClick={handleAesDecrypt}>
-                    Decrypt
-                  </Button>
-                }
-              </div>
+              <AES/>
             </CardContent>
           </Card>
         </TabsContent>
---
+++ encryption-frontend/src/components/aes.tsx
@@ -0,0 +1,253 @@
+import { Input, TextArea } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import { Button } from "@/components/ui/button";
+
+import { useState } from "react";
+import { aesEncrypt, aesDecrypt, printBlock } from "@/utils/aes";
+
+
+
+export function AES() {
+  const [aesKey, setAesKey] = useState("");
+  const [aesKeyInputStatus, setAesKeyInputStatus] = useState("");
+  const [aesDataInputStatus, setAesDataInputStatus] = useState("");
+  const [aesKeyWarning, setAesKeyWarning] = useState(false);
+  const [aesKeySize, setAesKeySize] = useState(128);
+  const [blockMode, setBlockMode] = useState(0);
+  const [aesData, setAesData] = useState("");
+  const [aesDataType, setAesDataType] = useState("ascii");
+  const [aesResult, setAesResult] = useState([] as Array<string>);
+
+  const handleAesKeyEnter = (value: string) => {
+    setAesKey(value);
+    if (value.length == 0) {
+      setAesKeyInputStatus("");
+      setAesKeyWarning(false);
+      return;
+    }
+    let len = 0;
+    if (value.startsWith("0x")) {
+      let valid = "0123456789abcdefABCDEF _-\n"
+      for (let c of value.slice(2)) {
+        if (!valid.includes(c)) {
+          setAesKeyInputStatus("Character '" + c + "' is invalid for hexadecimal input");
+          return;
+        } else if (c != ' ' && c != '_' && c != '-' && c != '\n') {
+          ++len;
+        }
+      }
+    } else if (value.startsWith("0b")) {
+      let valid = "01 _-\n"
+      for (let c of value.slice(2)) {
+        if (!valid.includes(c)) {
+          setAesKeyInputStatus("Character '" + c + "' is invalid for binary input");
+          return;
+        } else if (c != ' ' && c != '_' && c != '-' && c != '\n') {
+          ++len;
+        }     
+      }
+    } else {
+      setAesKeyInputStatus("Key must start with 0b (for binary) or 0x (for hexadecimal)");
+      return;
+    }
+    if (value.startsWith("0x")) {
+      if (len < aesKeySize / 4) {
+        setAesKeyWarning(true);
+        setAesKeyInputStatus("");
+      } else if (len == aesKeySize / 4) {
+        setAesKeyWarning(false);
+        setAesKeyInputStatus("");
+      } else {
+        setAesKeyWarning(false);
+        setAesKeyInputStatus("Key cannot exceed " + aesKeySize.toString() + " bits");
+      }
+    } else if (value.startsWith("0b")) {
+      if (len < aesKeySize) {
+        setAesKeyWarning(true);
+        setAesKeyInputStatus("");
+      } else if (len == aesKeySize) {
+        setAesKeyWarning(false);
+        setAesKeyInputStatus("");
+      } else {
+        setAesKeyWarning(false);
+        setAesKeyInputStatus("Key cannot exceed " + aesKeySize.toString() + " bits");
+      }
+
+    } else {
+      setAesKeyWarning(false);
+      setAesKeyInputStatus("");
+    }
+  }
+
+  const handleAesEncrypt = () => {
+    // Step 1: Turn the key string into a bigint
+    let whitespace = " _-\n";
+    let bin = false;
+    if (aesKey.startsWith("0x")) {
+      bin = false;
+    } else {
+      bin = true;
+    }
+    let key: bigint = BigInt(0);
+    for (let i=2; i < aesKey.length; ++i) {
+      for (; i < aesKey.length && whitespace.includes(aesKey[i]); ++i) {}
+      if (i >= aesKey.length) {break;}
+      if (bin) {
+        key <<= BigInt(1);
+        if (aesKey[i] == '1') key |= BigInt(1);
+      } else {
+        key <<= BigInt(4);
+        key |= BigInt(Number("0x" + aesKey[i]));
+      }
+    }
+    // Step 2: Turn the data string into an array of bytes
+    let data: number[] = [];
+    if (aesDataType == 'ascii') {
+      data = aesData.split("").map((x) => x.charCodeAt(0));
+    } else {
+      if (aesData.startsWith("0b")) {
+        for (let i=2; i < aesData.length; i += 8) {
+          data.push(Number("0b" + aesData.slice(i, i+8)));
+        }
+      } else {
+        for (let i=2; i < aesData.length; i += 2) {
+          data.push(Number("0x" + aesData.slice(i, i+2)));
+        }
+      }
+    }
+    // Step 3: Do the encryption
+    aesEncrypt(data, blockMode, aesKeySize, key);
+    // Step 4: Print the encrypted blocks
+    let res: string[] = [];
+    for (let i=0; i < (data.length / 16); ++i) {
+      res.push(printBlock(data.slice(i*16, (i+1)*16)));
+    }
+    setAesResult(res);
+  }
+
+  const handleAesDecrypt = () => {
+    // Step 1: Turn the key string into a bigint
+    let whitespace = " _-\n";
+    let bin = false;
+    if (aesKey.startsWith("0x")) {
+      bin = false;
+    } else {
+      bin = true;
+    }
+    let key: bigint = BigInt(0);
+    for (let i=2; i < aesKey.length; ++i) {
+      for (; i < aesKey.length && whitespace.includes(aesKey[i]); ++i) {}
+      if (i >= aesKey.length) {break;}
+      if (bin) {
+        key <<= BigInt(1);
+        if (aesKey[i] == '1') key |= BigInt(1);
+      } else {
+        key <<= BigInt(4);
+        key |= BigInt(Number("0x" + aesKey[i]));
+      }
+    }
+    // Step 2: Turn the data string into an array of bytes
+    let data: number[] = [];
+    if (aesData.startsWith("0b")) {
+      for (let i=2; i < aesData.length; i += 8) {
+        data.push(Number("0b" + aesData.slice(i, i+8)));
+      }
+    } else {
+      for (let i=2; i < aesData.length; i += 2) {
+        data.push(Number("0x" + aesData.slice(i, i+2)));
+      }
+    }
+    // Step 3: Decrypt
+
+  }
+
+  return (
+    <div className="space-y-4">
+      <div className="space-y-2">
+        <Label htmlFor="aes-type" className="block">AES Key Size</Label>
+        <select 
+          className="bg-muted px-2 py-2 outline"
+          onChange={(e) => setAesKeySize(Number(e.target.value))}
+        >
+          <option value="128">AES-128</option>
+          <option value="192">AES-192</option>
+          <option value="256">AES-256</option>
+        </select>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="aes-mode" className="block">AES Block Mode</Label>
+        <select 
+          className="bg-muted px-2 py-2 outline"
+          onChange={(e) => setBlockMode(Number(e.target.value))}
+        >
+          <option value="0">Electronic Codebook</option>
+          <option value="1">Cipherblock Chaining</option>
+          <option value="2">Counter</option>
+        </select>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="aes-datatype" className="block">Data Type</Label>
+        <select 
+          className="bg-muted px-2 py-2 outline"
+          onChange={(e) => setAesDataType(e.target.value)}
+        >
+          <option value="ascii">ASCII Text</option>
+          <option value="binary">Binary</option>
+        </select>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="aes-key">AES Key</Label>
+        <TextArea
+          id="aes-key"
+          value={aesKey}
+          onChange={(e) => handleAesKeyEnter(e.target.value)}
+          placeholder="Preface binary with 0b and hex with 0x"
+          className={(aesKey.length > 0) ? "font-mono" : ""}
+        />
+        {aesKeyInputStatus.length > 0 && (
+          <div>
+            <p className="text-red-700 text-sm">{aesKeyInputStatus}</p>
+          </div>
+        )}
+        {aesKeyWarning && aesKeyInputStatus.length == 0 && (
+          <div>
+            <p className="text-yellow-700 text-sm">Keys under {aesKeySize} bits will be left-padded with zeros</p>
+          </div>
+        )}
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="aes-data">Data</Label>
+        <TextArea
+          id="aes-data"
+          value={aesData}
+          onChange={(e) => setAesData(e.target.value)}
+          className={(aesDataType == "binary" && aesData.length > 0) ? "font-mono" : ""}
+          placeholder={
+            (aesDataType == "binary") ?
+              "Enter hex (0x) or binary (0b) data" :
+              "Enter text to encrypt here"
+          }
+        />
+      </div>
+      <div className="flex gap-2">
+        { (aesKeyInputStatus.length == 0) &&
+          <Button onClick={handleAesEncrypt}>Encrypt</Button>
+        }
+        { (aesDataType != "ascii") &&
+          (aesKeyInputStatus.length == 0) &&
+          <Button variant="outline" onClick={handleAesDecrypt}>
+            Decrypt
+          </Button>
+        }
+      </div>
+      {aesResult && (
+        <div className="mt-4 p-4 bg-muted rounded-md">
+          <p className="text-lg font-semibold underline">Result:</p>
+          {aesResult.map((line, index) : any => {
+            return <p className="font-mono" key={index}>{line}</p>;
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
